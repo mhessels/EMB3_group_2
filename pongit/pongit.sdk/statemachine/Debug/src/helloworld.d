29c74780b8f2014b7dcb0fc3be9e864ef4432abf
Matthias <mahes12@student.sdu.dk>
2016-05-26 23:43:45 +0200

    What i have sofar

---
+++ pongit/pongit.sdk/statemachine/Debug/src/helloworld.d
@@ -0,0 +1,48 @@
+src/helloworld.o src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xstatus.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xstatus.h:
---
+++ pongit/pongit.sdk/statemachine/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.d: ../src/platform.c \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../statemachine_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../statemachine_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
